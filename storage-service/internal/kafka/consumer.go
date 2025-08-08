b5f15bfd5784d3a3afd7c1d32eac83a3545c284f
deimossy <deimossy@student.21-school.ru>
2025-08-09 03:39:40 +0500

    feat(storage-service): implement Kafka consumer to read messages and forward to service

--- storage-service/internal/kafka/consumer.go
+++ storage-service/internal/kafka/consumer.go
@@ -1,2 +1,43 @@
 package kafka
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/badtripdude/gh-analyzer/internal/app"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Consumer struct {
+	reader  *kafka.Reader
+	service *app.Service
+}
+
+func NewConsumer(brokers []string, topic, groupID string, s *app.Service) *Consumer {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  brokers,
+		Topic:    topic,
+		GroupID:  groupID,
+		MinBytes: 10e3,
+		MaxBytes: 10e6,
+	})
+
+	return &Consumer{reader: r, service: s}
+}
+
+func (c *Consumer) Start(ctx context.Context) error {
+	for {
+		m, err := c.reader.ReadMessage(ctx)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Got message: %s\n", string(m.Value))
+
+		if err := c.service.HandleAnalysisResults(m.Value); err != nil {
+			log.Printf("Error processing message: %v", err)
+		}
+	}
+}
